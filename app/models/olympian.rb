8e8fcfac755355ff7ba98c2de8ec0e53e6aac111
Graham Thompson <grwthomps@gmail.com>
2020-03-02 15:49:33 -0700

    Add validations for Olympian model

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,2 +1,16 @@
 class Olympian < ApplicationRecord
+  validates_presence_of :name,
+                        :sex,
+                        :age,
+                        :height,
+                        :weight,
+                        :team,
+                        :games,
+                        :sport,
+                        :event,
+                        :medal
+
+  validates_numericality_of :age
+  validates_numericality_of :height
+  validates_numericality_of :weight
 end
