72207f34879d1d367f6f064ac5ec5de6a7f8928a
Graham Thompson <grwthomps@gmail.com>
2020-03-02 18:09:02 -0700

    Add many_to_many relationship to olympian and event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :sport
+
+  has_many :olympian_events
+  has_many :olympians, through: :olympian_events
 end
--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,2 +1,7 @@
 class Olympian < ApplicationRecord
+  belongs_to :team
+  belongs_to :sport
+
+  has_many :olympian_events
+  has_many :events, through: :olympian_events
 end
