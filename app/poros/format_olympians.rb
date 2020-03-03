083ba7515a00e737920ba4b53a99ec09638e80f1
Graham Thompson <grwthomps@gmail.com>
2020-03-03 15:47:00 -0700

    Add result and single_olympian methods to format_olymians poro

--- app/poros/format_olympians.rb
+++ app/poros/format_olympians.rb
@@ -1,4 +1,16 @@
 class FormatOlympians
+  def self.result(age)
+    if age.nil?
+      all_olympians
+    elsif age == "youngest"
+      single_olympian(Olympian.youngest)
+    elsif age == "oldest"
+      single_olympian(Olympian.oldest)
+    end
+  end
+
+  private
+
   def self.all_olympians
     Olympian.all.map do |olympian|
       {
@@ -9,5 +21,15 @@ class FormatOlympians
         "total_medals_won": olympian.total_medals_won
       }
     end
+  end
+
+  def self.single_olympian(olympian)
+    [{
+      "name": olympian.name,
+      "team": olympian.team.country,
+      "age": olympian.age,
+      "sport": olympian.sport.name,
+      "total_medals_won": olympian.total_medals_won
+    }]
   end
 end
