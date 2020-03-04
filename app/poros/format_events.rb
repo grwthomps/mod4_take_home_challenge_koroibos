3911e51b0ece5ce70d8449a8851b0bdd064c593d
Graham Thompson <grwthomps@gmail.com>
2020-03-03 21:35:34 -0700

    Expand format_events poro to format medalists

--- app/poros/format_events.rb
+++ app/poros/format_events.rb
@@ -8,11 +8,36 @@ class FormatEvents
     end
   end
 
+  def self.by_medalists(id)
+    event = Event.find(id)
+    medalists = event.find_medalists
+
+    if !medalists.empty?
+      medalists = format_medalists(medalists)
+    end
+
+    {
+      "event": event.name,
+      "medalists": medalists
+    }
+  end
+
   private
 
   def self.get_event_names(sport)
     sport.events.order(:name).map do |event|
       event.name
+    end
+  end
+
+  def self.format_medalists(medalists)
+    medalists.map do |medalist|
+      {
+        "name": medalist.olympian.name,
+        "team": medalist.olympian.team.country,
+        "age": medalist.olympian.age,
+        "medal": medalist.medal
+      }
     end
   end
 end
